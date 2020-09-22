bdbf641dabb720ce1e59e6b31a9e76d7cbaccbba
redlafougere <69895192+redlafougere@users.noreply.github.com>
2020-09-22 09:03:37 +0200

    push

--- MA_fonctions_generales.php
+++ MA_fonctions_generales.php
@@ -15,7 +15,7 @@ function setHeaderNoCache($title)
 	echo "<meta http-equiv=\"Pragma\" content=\"no-cache\" />\n";
 	echo "<meta http-equiv=\"Expires\" content=\"0\" />\n";
 	echo "<link href=\"annonce.css\" rel=\"stylesheet\">\n";
-	echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"css2.css\">\n";
+
 	//echo "<link href=\"MA_CSS.css\" rel=\"stylesheet\">\n";
 	echo "<link href='http://fonts.googleapis.com/css?family=Racing+Sans+One' rel='stylesheet' type='text/css'>\n";
     echo "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">\n";
